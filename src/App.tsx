44874d9556cbd66a2bd11566878b040c9dfa4e2b
Ivan <adamovskij00@mail.ru>
2022-03-29 22:55:06 +0300

    add Wrapper, Container SC

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import './App.css';
 import {currencyExchange} from './bll/dataLogic';
 import {CurrencyExContainer} from "./components/CurrencyExchange/CurrencyExContainer";
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+const Container = styled.div`
+  min-width: 460px;
+  max-width: 500px;
+  margin: 50px auto 0 auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <CurrencyExContainer store={currencyExchange}/>
-    </div>
+    <Wrapper>
+      <Container>
+        <CurrencyExContainer store={currencyExchange}/>
+      </Container>
+    </Wrapper>
   );
 }
 
