96216eab2912136d04800f6e8aa36e8908751a85
Ivan <adamovskij00@mail.ru>
2022-03-30 15:45:01 +0300

    add upd background style

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
 `
 const Wrapper = styled.section`
   min-width: 540px;
   max-width: 540px;
+  height: 100vh;
   padding: 4em;
   margin: 0 auto;
+  background: linear-gradient(45deg, #EECFBA, #C5DDE8) no-repeat center 100%;
+  background-size: cover;
 `;
 
 export const App = () => {
