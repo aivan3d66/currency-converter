4801654c5ab441daafd2a8168a7715aa99198f5b
Ivan <adamovskij00@mail.ru>
2022-03-30 10:23:55 +0300

    add upd Wrapper width

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,9 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background: linear-gradient(45deg, #EECFBA, #C5DDE8);
 `
 const Wrapper = styled.section`
-  min-width: 470px;
+  min-width: 540px;
   max-width: 540px;
   padding: 4em;
   margin: 0 auto;
